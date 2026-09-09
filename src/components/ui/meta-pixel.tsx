eb236ee8e3d4b303e18fccadd84121601f1611c4
lucas all lucky <luckylucasall@gmail.com>
2026-09-09 10:01:12 -0300

    Add files via upload

---
+++ src/components/ui/meta-pixel.tsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import { useLocation } from "@tanstack/react-router";
+
+declare global {
+  interface Window {
+    fbq?: ((...args: unknown[]) => void) & {
+      callMethod?: (...args: unknown[]) => void;
+      queue?: unknown[][];
+      version?: string;
+      loaded?: boolean;
+    };
+    _fbq?: Window["fbq"];
+  }
+}
+
+const pixelId = import.meta.env.VITE_META_PIXEL_ID || "2345117496319374";
+
+export function MetaPixel() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!pixelId || typeof window === "undefined") return;
+
+    if (!window.fbq) {
+      const fbq = ((...args: unknown[]) => {
+        if (fbq.callMethod) {
+          fbq.callMethod(...args);
+        } else {
+          fbq.queue?.push(args);
+        }
+      }) as NonNullable<Window["fbq"]>;
+
+      fbq.queue = [];
+      fbq.version = "2.0";
+      fbq.loaded = true;
+      window.fbq = fbq;
+      window._fbq = fbq;
+
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = "https://connect.facebook.net/en_US/fbevents.js";
+      document.head.appendChild(script);
+
+      window.fbq("init", pixelId);
+    }
+
+    window.fbq("track", "PageView");
+  }, [location.pathname]);
+
+  return (
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style={{ display: "none" }}
+        src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+        alt=""
+      />
+    </noscript>
+  );
+}
